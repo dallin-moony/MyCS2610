2385c08eb8ab54e6592fca0d4a0c9428d36b9902
Dallin Matthew Moon <A02338740@usu.edu>
2025-10-09 17:22:34 -0600

    remove TODOS project

--- TODOS/TODOS/TODOS_App/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Todo(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    contents = models.TextField()
-    is_completed = models.BooleanField(default=False)
-
-    
--- TODOS/TODOS/TODOS_App/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('index/', views.index, name='index_alt'),
-    path('create/', views.create_todo, name='create_todo'),
-]
